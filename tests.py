f8fecba87e25413aabe4c67a89d5a4ba86346103
Dustin Cross <153316602+Ducross8525@users.noreply.github.com>
2024-10-21 12:30:18 -0500

    Commit2:
    l2d

--- tests.py
+++ tests.py
@@ -32,7 +32,7 @@ class TestInit(unittest.TestCase):
      #if the inputs share a factor, is the fraction reduced? i.e. 2/4 = 1/2
 
 class TestStr(unittest.TestCase):
-  def test_displayfraction(self):
+  def test_displayFraction(self):
     a = Fraction(1,2)
     self.assertEqual(" 1/2 ",a.__str__(),"fraction not displayed correctly")
   def test_displayInt(self):
@@ -45,33 +45,55 @@ class TestStr(unittest.TestCase):
     #if the fraction is negative, is it possible to erroneously have it display 1/-2, vs -1/2?
     
 class TestFloat(unittest.TestCase):
-    c = fraction(3,4)
-    self.assertEqual(.75,c.__float__())
-  #should return a float of numerator/denomenator
-  #negative fraction negative float
+  def test_defDen(self):
+    c = Fraction(2)
+    self.assertTrue(isinstance(c.__float__(),float),"float must return a float")
+  def test_nonDefDen(self):
+    c = Fraction(3,4)
+    self.assertEqual(.75,c.__float__(),"float request should return the numerator divided by the denominator")
+    #should return a float of numerator/denominator
+  def test_negFloat(self):
+    c = Fraction(-1,2)
+    self.assertLess(c.__float__(),0,"there should be a negative float if the fraction is negative")
+    #negative fraction negative float
 
 class TestAdd(unittest.TestCase):
-  pass
-  #should raise type error when passed a nonfraction
-  #should return a fraction
-  #returned faction should be in lowest common form
+  def test_type(self):
+    c=Fraction()
+    with self.assertRaises(TypeError,msg="add method should raise a type error with incorrect argument type"):
+      c.__add__(0)
+      c.__add__("a")
+    #should raise type error when passed a non-fraction
+  def test_return(self):
+    c=Fraction()
+    b=Fraction(1,2)
+    a=c.__add__(b)
+    self.assertTrue(isinstance(a,Fraction),"add method should return a Fraction")
+    #should return a fraction
+  def test_fact(self):
+    c=Fraction(1,2)
+    b=Fraction(1,2)
+    a=c.__add__(b)
+    self.assertEqual(1,a.numerator,"Numerator is not factored")
+    self.assertEqual(1,a.denominator,"Denominator is not factored")
+  #returned faction should be in the lowest common form
 
 class TestSub(unittest.TestCase):
   pass
-  #should raise type error when passed a nonfraction
+  #should raise type error when passed a non-fraction
   #should return a fraction
-  #returned faction should be in lowest common form
+  #returned faction should be in the lowest common form
 
 class TestMul(unittest.TestCase):
   pass
-  #should raise type error when passed a nonfraction
+  #should raise type error when passed a non-fraction
   #should return a fraction
-  #returned faction should be in lowest common form
+  #returned faction should be in the lowest common form
   #negative guest negative result
 
 class TestDiv(unittest.TestCase):
   pass
-  #should raise type error when passed a nonfraction
+  #should raise type error when passed a non-fraction
   #should return a fraction
-  #returned faction should be in lowest common form
+  #returned faction should be in the lowest common form
   #negative guest negative result
